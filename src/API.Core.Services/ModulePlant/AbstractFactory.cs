1dfaed9e94d82f009f9591104b437ccdc29c0add
kai_wang <871779972@qq.com>
2020-02-06 17:21:50 +0800

    定义抽象工厂类的基类

---
+++ src/API.Core.Services/ModulePlant/AbstractFactory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace API.Core.Services.ModulePlant.Factory
+{
+    //定义抽象工厂类
+    public abstract class AbstractFactory
+    {
+        public abstract object CreateBinFile();
+    }
+}
