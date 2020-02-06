4d1a668bd030c951aeeee2f86e391db538a1afc9
kai_wang <871779972@qq.com>
2020-02-06 19:01:05 +0800

    更改文件夹名

--- src/API.Core.Services/FindBinFile/BoxBinFile.cs
+++ src/API.Core.Services/FindBinFile/BoxBinFile.cs
@@ -1,11 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace API.Core.Services.ModulePlant
 {
     public class BoxBinFile:BInFile
     {
+        // Fsbox具体显示寻找Bin文件的函数
         public override void CreateBinFile()
         {
             Console.WriteLine("Box 创建bin文件");
--- src/API.Core.Services/FindBinFile/OsBinFile.cs
+++ src/API.Core.Services/FindBinFile/OsBinFile.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace API.Core.Services.ModulePlant
 {
     // os 创建bin文件的具体实现类
     public class OsBinFile : BInFile
     {
+        // os具体显示寻找Bin文件的函数
         public override void CreateBinFile()
         {
             Console.WriteLine("OS 创建bin文件");
