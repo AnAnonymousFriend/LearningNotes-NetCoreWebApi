40e5cc77fdce68c3b5a2e6f1f0dda5064ae3cfb4
kai_wang <871779972@qq.com>
2020-03-23 15:50:07 +0800

    尝试连接

--- src/Test/RabbitMQ_Test/Program.cs
+++ src/Test/RabbitMQ_Test/Program.cs
@@ -1,13 +1,56 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
 
 namespace RabbitMQ_Test
 {
     class Program
     {
-        // 生产者消费者
+        // 定义生产者
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 连接RabbitMQ
+            //ConnectionFactory factory = new ConnectionFactory
+            //{
+            //    UserName = "admin",
+            //    Password = "admin",
+            //    HostName = "http://127.0.0.1:15672/"
+            //};
+
+            //// 创建连接
+
+
+            var factory = new ConnectionFactory()
+            {
+                HostName = "192.168.1.121",
+                Port = 15672,
+                UserName = "admin",
+                Password = "admin"
+            };
+            IConnection conn = factory.CreateConnection();
+
+            var connection = factory.CreateConnection();
+
+            var channel = connection.CreateModel();
+
+            //声明一个队列
+            channel.QueueDeclare("hello", false, false, false, null);
+
+            Console.WriteLine("\nRabbitMQ连接成功，请输入消息，输入exit退出！");
+
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+
+                var sendBytes = Encoding.UTF8.GetBytes(input);
+                //发布消息
+                channel.BasicPublish("", "hello", null, sendBytes);
+
+            } while (input.Trim().ToLower() != "exit");
+            channel.Close();
+            connection.Close();
+
         }
     }
 }
