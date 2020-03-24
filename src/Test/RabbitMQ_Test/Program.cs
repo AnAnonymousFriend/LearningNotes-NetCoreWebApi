ecbe565efea3402e62517301334c74a031613893
kai_wang <871779972@qq.com>
2020-03-24 11:20:59 +0800

    定义消费者

--- src/Test/RabbitMQ_Test/Program.cs
+++ src/Test/RabbitMQ_Test/Program.cs
@@ -9,10 +9,6 @@ namespace RabbitMQ_Test
         // 定义生产者
         static void Main(string[] args)
         {
-           
-          
-
-
             //1.1.实例化连接工厂
             var factory = new ConnectionFactory() { HostName = "localhost" };
             var connection = factory.CreateConnection();
@@ -37,22 +33,7 @@ namespace RabbitMQ_Test
             connection.Close();
 
 
-            ////2. 建立连接
-            //using (var connection = factory.CreateConnection())
-            //{
-            //    //3. 创建信道
-            //    using (var channel = connection.CreateModel())
-            //    {
-            //        //4. 申明队列
-            //        channel.QueueDeclare(queue: "hello", durable: false, exclusive: false, autoDelete: false, arguments: null);
-            //        //5. 构建byte消息数据包
-            //        string message = args.Length > 0 ? args[0] : "Hello RabbitMQ!";
-            //        var body = Encoding.UTF8.GetBytes(message);
-            //        //6. 发送数据包
-            //        channel.BasicPublish(exchange: "", routingKey: "hello", basicProperties: null, body: body);
-            //        Console.WriteLine(" [x] Sent {0}", message);
-            //    }
-            //}
+        
 
         }
     }
